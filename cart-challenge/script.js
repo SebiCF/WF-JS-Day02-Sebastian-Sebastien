cbae0d0d5c9565b67f77ec447c134556932e7c93
Sébastien Pflanzl <sv.pflanzl@gmail.com>
2019-10-07 13:17:40 +0200

    add cart stuff

--- cart-challenge/script.js
+++ cart-challenge/script.js
@@ -1,3 +1,5 @@
+var items = {}
+
 function closeCart() {
   let cart = document.getElementById('cart-visible')
   if(!cart) return
@@ -8,4 +10,44 @@ function openCart() {
   let cart = document.getElementById('cart-invisible')
   if(!cart) return
   cart.id = 'cart-visible'
+}
+
+function makeItem(name, price, imgurl) {
+  let item = document.createElement('div')
+  let itemName = document.createElement('p')
+  let img  = document.createElement('img')
+  let addToCartButton = document.createElement('button')
+
+  img.src = imgurl
+  item.className = "item"
+  itemName.innerHTML = `${name}, ${price} $`
+
+  addToCartButton.innerHTML = '🛒'
+  addToCartButton.addEventListener('click', function() { addToCart(item) })
+
+  item.appendChild(img)
+  item.appendChild(itemName)
+  item.appendChild(addToCartButton)
+
+  return item
+}
+
+function addToCart(item) {
+  let cart_contents = document.getElementById('contents')
+  cart_contents.appendChild(item)
+}
+
+function fillProductList() {
+  let items = [
+    makeItem("a bottle of dirty water", 99, "#"),
+    makeItem("Harry Potter Vol 8", 999, "#"),
+    makeItem("cat", 5, "#"),
+    makeItem("dog", 7, "#")
+  ]
+
+  let product_list = document.getElementById('products')
+
+  for(i=0;i<items.length;i++) {
+    product_list.appendChild(items[i])
+  }
 }
