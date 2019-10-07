bd770904e51d78862a3cc0ea48b2e6ac585308f4
Sebastian Malcher <sebastianlukasmalcher@gmail.com>
2019-10-07 13:47:10 +0200

    Finishes challenge1

--- cart-challenge/script.js
+++ cart-challenge/script.js
@@ -1,54 +1,84 @@
 var items = {}
 
 function closeCart() {
-  let cart = document.getElementById('cart-visible')
-  if(!cart) return
-  cart.id = 'cart-invisible'
+    let cart = document.getElementById('cart-visible')
+    if (!cart) return
+    cart.id = 'cart-invisible'
 }
 
 function openCart() {
-  let cart = document.getElementById('cart-invisible')
-  if(!cart) return
-  cart.id = 'cart-visible'
+    let cart = document.getElementById('cart-invisible')
+    if (!cart) return
+    cart.id = 'cart-visible'
 }
 
 
 function makeItem(name, price, imgurl) {
-  let item = document.createElement('div')
-  let itemName = document.createElement('p')
-  let img  = document.createElement('img')
-  let addToCartButton = document.createElement('button')
+    let item = document.createElement('div')
+    let itemName = document.createElement('p')
+    let img = document.createElement('img')
+    let addToCartButton = document.createElement('button')
 
-  img.src = imgurl
-  item.className = "item"
-  itemName.innerHTML = `${name}, ${price} $`
+    img.src = imgurl
+    item.className = "item"
+    itemName.innerHTML = `${name}, ${price} $`
 
-  addToCartButton.innerHTML = '🛒'
-  addToCartButton.addEventListener('click', function() { addToCart(item) })
+    addToCartButton.innerHTML = '🛒'
+    addToCartButton.addEventListener('click', function() { addToCart(name, price, imgurl) })
 
-  item.appendChild(img)
-  item.appendChild(itemName)
-  item.appendChild(addToCartButton)
+    item.appendChild(img)
+    item.appendChild(itemName)
+    item.appendChild(addToCartButton)
 
-  return item
+    return item
 }
 
-function addToCart(item) {
-  let cart_contents = document.getElementById('contents')
-  cart_contents.appendChild(item)
+
+function makeCartItem(name, price, imgurl) {
+    let item = document.createElement('div')
+    let itemName = document.createElement('p')
+    let img = document.createElement('img')
+    let addToCartButton = document.createElement('button')
+
+    img.src = imgurl
+    item.className = "item"
+    itemName.innerHTML = `${name}, ${price} $`
+
+    addToCartButton.innerHTML = 'X'
+    addToCartButton.addEventListener('click', function() { removeFromCart(item) })
+
+    item.appendChild(img)
+    item.appendChild(itemName)
+    item.appendChild(addToCartButton)
+
+    return item
 }
+
+function addToCart(name, price, imgurl) {
+    let cartItem = makeCartItem(name, price, imgurl);
+    let cart_contents = document.getElementById('contents')
+    cart_contents.appendChild(cartItem)
+    openCart();
+}
+
+function removeFromCart(item) {
+    let cart_contents = document.getElementById('contents')
+    cart_contents.removeChild(item)
+}
+
+
 
 function fillProductList() {
-  let items = [
-    makeItem("a bottle of dirty water", 99, "#"),
-    makeItem("Harry Potter Vol 8", 999, "#"),
-    makeItem("cat", 5, "#"),
-    makeItem("dog", 7, "#")
-  ]
+    let items = [
+        makeItem("a bottle of dirty water", 99, "#"),
+        makeItem("Harry Potter Vol 8", 999, "#"),
+        makeItem("cat", 5, "#"),
+        makeItem("dog", 7, "#")
+    ]
 
-  let product_list = document.getElementById('products')
+    let product_list = document.getElementById('products')
 
-  for(i=0;i<items.length;i++) {
-    product_list.appendChild(items[i])
-  }
+    for (i = 0; i < items.length; i++) {
+        product_list.appendChild(items[i])
+    }
 }
